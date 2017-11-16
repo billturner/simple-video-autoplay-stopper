63b8fab9799297e85b619bab17e03489c684c293
Bill Turner <billturner@gmail.com>
2017-11-16 11:18:31 -0800

    Found another CSS class used for video players

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 // in case there are multiple videos, let's get all of 'em
-const playerWrappers = document.body.querySelectorAll('.media__video');
+const playerWrappers = [
+  ...document.body.querySelectorAll('.media__video'),
+  ...document.body.querySelectorAll('.cnnplayer')
+];
 
 // now, delete them from the DOM
 playerWrappers.forEach(player => {
