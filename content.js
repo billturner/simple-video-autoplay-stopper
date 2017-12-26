e6736fda6c7b87fee9713da29df7a36528f89359
Bill Turner <billturner@gmail.com>
2017-12-26 13:54:30 -0500

    Adding some BBC wrappers to hide

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
 // in case there are multiple videos, let's get all of 'em
 const playerSelectors = [
+  // CNN
   '.media__video',
-  '.cnnplayer'
+  '.cnnplayer',
+  // BBC
+  '.vxp-media__player',
+  '.mediaContainer'
 ];
 
 const playerElements = playerSelectors.reduce((acc, selector) => {
